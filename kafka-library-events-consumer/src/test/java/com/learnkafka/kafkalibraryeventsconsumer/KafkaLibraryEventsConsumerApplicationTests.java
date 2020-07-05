a69c303fcff1d3d624eeef6c8cc353ad6998706c
Deepan Reguraman <Deepan.Reguraman@centurylink.com>
2020-07-05 20:12:54 +0530

    consumer

---
+++ kafka-library-events-consumer/src/test/java/com/learnkafka/kafkalibraryeventsconsumer/KafkaLibraryEventsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnkafka.kafkalibraryeventsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLibraryEventsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
